25b68e7563a7ad0b68d401effa8ec4499d5cbe76
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-21 01:40:55 +0200

    something

--- src/process_data.py
+++ src/process_data.py
@@ -1,13 +1,15 @@
 import numpy as np
-import csv
 import openai
 import yaml
+from bs4 import BeautifulSoup
+import os
 
 #test imports
 import document_parser
 
 #setup
 API_KEY = ""
+DATA_PATH = "./data/"
 START_PROMPT = """
 Mohl by jsi fungovat jako model co bude hodnotit školu ve škále od 0 do 10 podle textu co ti zde zadám? Zpráva vždy bude začínat slovem analyzuj. Přičemž odpověď musí za každých okolností vždy být pouze to číslo
 """
@@ -35,13 +37,46 @@ def set_classification():
     #a prompt to set up chatGPT to classification from 0 to 10
     prompt(START_PROMPT)
 
-def main():
-    pass
+def load_all_data():
+    for i, filename in enumerate(os.listdir(DATA_PATH)):
+        file_path = os.path.join(DATA_PATH, filename)
+        # checking if it is a file
+        if os.path.isfile(file_path) and ".gitignore" not in file_path:
+            
+            out_array = []
+            # Creating own dataset
+            # | label: 
+            # | array: 
+        
+            with open(file_path, 'r') as f:
+                file_data = f.read()
+
+                Bs_data = BeautifulSoup(file_data, "xml")
+
+                data_div = Bs_data.find("data")
+                data = data_div.find_all("udaj")
+                print(len(data))
+
+                out = []
+                for iy in range(int(len(data) / 4)): #4 columns
+                    row = []
+
+                    for ix in range(4):
+                        value = data[iy * ix].find("hod").text
+
+                        row.append(value)
+
+                    out.append(row)
+
+                print(out)
 
 if __name__ == "__main__":
+    """
     #just for testing
     set_classification()
     chat_in = document_parser.parse_doc()
     response = prompt(chat_in)
 
     print(response)
+    """
+    load_all_data()
